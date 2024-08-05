36679125b1d0d1e8792a0fd1c1f9c06b9bee1234
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2024-08-05 15:49:39 -0400

    Added recursive method of DFS

--- graph-theory/README.md
+++ graph-theory/README.md
@@ -1,4 +1,4 @@
 # How to interpret a adjacency matrix
-(https://www.youtube.com/watch?v=PMMc4VsIacU&list=PLpXOY-RxVRTPPVLBP6-sz6CMWxhtrI-v_&index=3)
-![alt text](image.png)
-Each row shows where it connects with:
+(https://www.youtube.com/watch?v=LFKZLXVO-Dg&list=PLpXOY-RxVRTPPVLBP6-sz6CMWxhtrI-v_)
+
+Use manim to render the graph...
--- graph-theory/depth-first-search/recursive/recursive.py
+++ graph-theory/depth-first-search/recursive/recursive.py
@@ -1,6 +1,33 @@
-from graph import Graph
+# DFS IMPLEMENTATION 1 (https://www.youtube.com/watch?v=PMMc4VsIacU&list=PLpXOY-RxVRTPPVLBP6-sz6CMWxhtrI-v_&index=3)
 
+# Setup graph (must be consecutive numbers)
+graph = []
 
-g = Graph(5)
-g.add_edge(0, 1)
-g.add_edge(0, 2)
+# Each array item corresponds to where each index connects to
+graph.append([1, 2, 3])
+graph.append([0, 3])
+graph.append([0, 3])
+graph.append([0, 1, 2, 4])
+graph.append([3])
+
+# Run DFS
+marked = [False] * len(graph)
+print(marked, len(marked))
+
+def dfs(graph, givenVertex):
+    print(givenVertex)
+    # "visit" givenVertex
+    neighbors = graph[givenVertex]
+    # marked the vertex as seen before
+    marked[givenVertex] = True
+    
+    # for each vertex
+    for v in neighbors:
+        if not marked[v]:
+            dfs(graph, v)
+
+dfs(graph, 0)
+print(marked)
+
+## VISUALIZING / RENDERING GRAPH ON SCREEN ##
+
